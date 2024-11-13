4434a5fcc90b518f12df3e7fdc814c10a1e7e332
Design And Creation <romain.doyen97@gmail.com>
2024-11-13 16:34:42 +0400

    Inserting data from a Seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $admin = new Role();
+        // $admin->libelle = 'Administrateur';
+        // $admin->save();
+
+        // $client = new Role();
+        // $client->libelle = 'Client';
+        // $client->save();
+    }
+}
