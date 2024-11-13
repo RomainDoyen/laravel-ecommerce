8eaf91d905204d628486160a12203dd98e3f2a9d
Design And Creation <romain.doyen97@gmail.com>
2024-11-13 14:52:40 +0400

    Creation of the login and registration page

--- resources/views/client/login.blade.php
+++ resources/views/client/login.blade.php
@@ -1,3 +1,39 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Connexion</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="{{ asset('assets/css/form-contact.css') }}">
+	</head>
+	<body>
+		<div class="wrapper" style="background-image: url('{{ asset('assets/images/bg-registration-form-1.jpg') }}');">
+            <div class="backhome">
+                <a href="{{ route('front.index') }}" class="bottom-text-w3ls">Revenir à la page d'accueil</a>
+            </div>    
+            <div class="inner">
+				<div class="image-holder">
+					<img src="{{ asset('assets/images/registration-form-1.jpg') }}" alt="">
+				</div>
+				<form action="">
+					<h3>Se connecter</h3>
+					<div class="form-wrapper">
+						<input type="text" name="email" placeholder="Adresse email" class="form-control">
+						<i class="zmdi zmdi-email"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" name="password" placeholder="Mot de passe" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<button>
+						Se connecter <i class="zmdi zmdi-arrow-right"></i>
+					</button>
+                    <br>
+                    <div class="links">
+                        <a href="{{ route('client.register') }}" class="bottom-text-w3ls">Pas de compte ? S'inscrire maintenant.</a>
+                    </div>
+                </form>
+			</div>
+		</div>
+	</body>
+</html>
--- resources/views/client/register.blade.php
+++ resources/views/client/register.blade.php
@@ -1,3 +1,48 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Inscription</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="{{ asset('assets/css/form-contact.css') }}">
+	</head>
+	<body>
+		<div class="wrapper" style="background-image: url('{{ asset('assets/images/bg-registration-form-1.jpg') }}');">
+			<div class="backhome">
+        <a href="{{ route('front.index') }}" class="bottom-text-w3ls">Revenir à la page d'accueil</a>
+      </div> 
+			<div class="inner">
+				<div class="image-holder">
+					<img src="{{ asset('assets/images/registration-form-1.jpg') }}" alt="">
+				</div>
+				<form action="">
+					<h3>S'Inscrire</h3>
+					<div class="form-group">
+						<input type="text" name="firstname" placeholder="Prénom" class="form-control">
+						<input type="text" name="lastname" placeholder="Nom" class="form-control">
+					</div>
+					<div class="form-wrapper">
+						<input type="text" name="email" placeholder="Adresse email" class="form-control">
+						<i class="zmdi zmdi-email"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" name="password" placeholder="Mot de passe" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<!-- <div class="form-wrapper">
+						<input type="password" placeholder="Confirm Password" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div> -->
+					<button>
+						S'inscrire <i class="zmdi zmdi-arrow-right"></i>
+					</button>
+					<br>
+					<div class="links">
+						<a href="{{ route('client.login') }}" class="bottom-text-w3ls">Vous avez un compte ? Se connecter maintenant.</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
