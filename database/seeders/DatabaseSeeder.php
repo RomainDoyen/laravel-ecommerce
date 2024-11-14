970796e4792d118397b9ddd54bdf93fa5cdedcfd
Design And Creation <romain.doyen97@gmail.com>
2024-11-14 09:49:07 +0400

    Product creation from a Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'nom' => 'Doe',
+            'prenom' => 'John',
+            'email' => 'johndoe@gmail.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
---
+++ database/seeders/ProduitSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Produit;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProduitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $prouit1 = new Produit();
+        $prouit1->titre = 'Produit 1';
+        $prouit1->description = 'Description du produit 1';
+        $prouit1->prix = 10;
+        $prouit1->save();
+
+        $prouit2 = new Produit();
+        $prouit2->titre = 'Produit 2';
+        $prouit2->description = 'Description du produit 2';
+        $prouit2->prix = 20;
+        $prouit2->image = 'assets/images/p3.png';
+        $prouit2->save();
+    }
+}
